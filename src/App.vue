c68e3931f2293efb8075d91cf978371a8beac7e7
liyueyaya <13526694128@163.com>
2019-06-23 16:38:14 +0800

    '登录进去后的整体布局-使得充满页面’

--- src/App.vue
+++ src/App.vue
@@ -18,5 +18,8 @@ export default {
 </script>
 
 <style lang="less" scoped>
+#app {
+  height: 100%;
+}
 
 </style>
--- src/views/home/layout/index.vue
+++ src/views/home/layout/index.vue
@@ -1,15 +1,43 @@
 <template>
-    <div id='app'>
-
-    </div>
+    <el-container>
+      <el-aside class="Aside" width="200px">Aside</el-aside>
+      <el-container>
+        <el-header class="Header">Header</el-header>
+        <el-main class="Main">Main</el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
+import AppAside from './components/AppAside.vue'
+import AppHead from './components/AppHead.vue'
 export default {
+  name: 'AppLayout',
+  components: {
+    AppAside,
+    AppHead
+  },
+  data () {
+    return {
+
+    }
+  }
 
 }
 </script>
 
 <style>
+.el-container {
+    height: 100%
+}
+.el-aside {
+    background-color: #D3DCE6
+}
+.el-header {
+    background-color: #B3C0D1
+}
+.el-main {
+    background-color:#666
+}
 
 </style>
