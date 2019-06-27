508496d5b4ada58894804b87a225e692c64fcf08
liyueyaya <13526694128@163.com>
2019-06-27 16:09:33 +0800

    '用户登录-发送验证码-使用极验API初始化验证码'

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -36,10 +36,9 @@
  </div>
 </template>
 
-<script src="gt.js"></script>
 <script>
 import axios from 'axios'
-import '@/vendor/gt' 
+import '@/vendor/gt'
 export default {
   name: 'AppLogin',
   data () {
@@ -59,30 +58,31 @@ export default {
       const { mobile } = this.form
       axios({
         method: 'GET',
-        url: `https://mock.boxuegu.com/mock/434/v1_0/captchas/${mobile}`
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
       }).then(res => {
         // console.log(res.data)
         // const data = res.data.data
         const { data } = res.data
         window.initGeetest({
-            // 以下配置参数来自服务端 SDK
-            gt: data.gt,
-            challenge: data.challenge,
-            offline: !data.success,
-            new_captcha: data.new_captcha,
-            product: 'bind'
+          // 以下配置参数来自服务端 SDK
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success,
+          new_captcha: data.new_captcha,
+          product: 'bind'
         }, function (captchaObj) {
-            // 这里可以调用验证实例 captchaObj 的实例方法
-            console.log(captchaObj)
-               captchaObj.onReady(function(){
-               //验证码ready之后才能调用verify方法显示验证码
-               captchaObj.verify();
-              }).onSuccess(function(){
-                    //your code
-                  console.log(aptchaObj.getValidate());
-              }).onError(function(){
-                    //your code
-              })
+          // 这里可以调用验证实例 captchaObj 的实例方法
+          // console.log(captchaObj)
+          captchaObj.onReady(function () {
+            // 验证码ready之后才能调用verify方法显示验证码
+            captchaObj.verify()
+          }).onSuccess(function () {
+            // your code
+            console.log(captchaObj.getValidate())
+           
+          }).onError(function () {
+            // your code
+          })
         })
       })
     }
